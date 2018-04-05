e997ebfd2af759240771d97e49c1f186aa69ad6c
asiaantczak <aasiaantczak@gmail.com>
2018-04-05 11:27:35 +0000

    add interactive menu

--- directory.rb
+++ directory.rb
@@ -1,3 +1,28 @@
+def interactive_menu
+  students = []
+  loop do
+  # 1. print the menu and ask the user what to do
+    puts "1. Input the students"
+    puts "2. Show the students"
+    puts "9. Exit"
+  # 2. read the input and save it into variable
+    selection = gets.chomp
+  # 3. do what the user has asked
+    case selection
+    when "1"
+      students = input_students
+    when "2"
+      print_header
+      print(students)
+      print_footer(students)
+    when "9"
+      exit
+    else
+      puts "I don't know what you meant, try again"
+    end
+  end
+end
+
 def input_students
   puts "Please enter the names of the students"
   puts "To finish, just enter return twice"
@@ -53,7 +78,6 @@ def print_by_cohorts(students)
     else
       cohorts[cohort] << name
     end
-     
   end
    
   cohorts.each do |cohort, name|
@@ -86,28 +110,4 @@ def print_footer(names)
   puts "Overall, we have #{names.count} great students"
 end
 
-students = input_students
-# nothing happens until we call the methods
-
-puts "Would you like to print standard students list, grouped by cohort or selected students? Enter 'all', 'cohort' or 'selected' "
-answer = gets.chomp
-  if answer == "all"
-    print_header
-    print(students)
-    print_footer(students)
-    print_by_length(students)
-  elsif answer == "selected"
-    print_header
-    print_by_letter(students)
-    print_by_length(students)
-  elsif answer == "cohort"
-    print_header
-    print_by_cohorts(students)
-    print_footer(students)
-  end
-
-
-
-
-
-
+interactive_menu
