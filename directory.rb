7c1c8eb65552718e10d0e6b45970a589c4311689
asiaantczak <aasiaantczak@gmail.com>
2018-04-05 12:35:03 +0000

    refactor code and put in comment additional methods

--- directory.rb
+++ directory.rb
@@ -1,56 +1,47 @@
-def interactive_menu
-  students = []
-  loop do
-  # 1. print the menu and ask the user what to do
-    puts "1. Input the students"
-    puts "2. Show the students"
-    puts "9. Exit"
-  # 2. read the input and save it into variable
-    selection = gets.chomp
-  # 3. do what the user has asked
-    case selection
-    when "1"
-      students = input_students
-    when "2"
-      print_header
-      print(students)
-      print_footer(students)
-    when "9"
-      exit
-    else
-      puts "I don't know what you meant, try again"
-    end
-  end
-end
+@students = []
 
 def input_students
   puts "Please enter the names of the students"
   puts "To finish, just enter return twice"
-  # create an empty array
-  students = []
-  # get the first name
   name = gets.chomp
-  cohort = gets.chomp
-  # while the name is not empty, repeat this code
+  
   while !name.empty? do
-    # add the student hash to the array
-    if cohort == "" || cohort == " "
-      students << {name: name, cohort: "April"}
-    else
-       students << {name: name, cohort: cohort.to_sym}
-    end
-    
-    if students.count == 1
-      puts "Now we have #{students.count} student"
-    else
-       puts "Now we have #{students.count} students"
-    end
-    # get another name from the user
+    @students << {name: name, cohort: :november}
+    puts "Now we have #{@students.count} student"
     name = gets.chomp
-    cohort = gets.chomp 
   end
-  # return the array of students
-  students
+end
+
+def interactive_menu
+  loop do
+    print_menu
+    process(gets.chomp)
+  end
+end
+
+def print_menu
+  puts "1. Input the students"
+  puts "2. Show the students"
+  puts "9. Exit"
+end
+
+def show_students
+  print_header
+  print_students_list
+  print_footer
+end
+
+def process(selection)
+  case selection
+    when "1"
+      input_students
+    when "2"
+      show_students
+    when "9"
+      exit
+    else
+      puts "I don't know what you meant, try again"
+  end
 end
 
 def print_header
@@ -58,10 +49,47 @@ def print_header
   puts "-------------------------------"
 end
 
-def print(students)
+def print_students_list
+  @students.each do |student|
+    puts "#{student[:name]} (#{student[:cohort]} cohort)"
+  end
+end
+
+def print_footer
+  puts "Overall, we have #{@students.count} great students"
+end
+
+interactive_menu
+
+# additional/ improved methods from excercises step8
+=begin
+def input_students
+  puts "Please enter the names of the students"
+  puts "To finish, just enter return twice"
+  name = gets.chomp
+  cohort = gets.chomp  
+  
+  while !name.empty? do
+    if cohort == "" || cohort == " "
+      @students << {name: name, cohort: "April"}
+    else
+       @students << {name: name, cohort: cohort.to_sym}
+    end
+    
+    if @students.count == 1
+      puts "Now we have #{@students.count} student"
+    else
+       puts "Now we have #{@students.count} students"
+    end
+    name = gets.chomp
+    cohort = gets.chomp 
+  end
+end
+
+def print_students_list
   i = 0
-  while i < students.length do
-    puts "#{i + 1}. #{students[i][:name]} (#{students[i][:cohort]} cohort)".center(60)
+  while i < @students.length do
+    puts "#{i + 1}. #{@students[i][:name]} (#{@students[i][:cohort]} cohort)".center(60)
     i += 1
   end
 end
@@ -105,9 +133,4 @@ def print_by_length(students)
     end
   end
 end
-
-def print_footer(names)
-  puts "Overall, we have #{names.count} great students"
-end
-
-interactive_menu
+=end
