b55080e51069d5c3dd6b8d60cd0d2d8eb3541c92
asiaantczak <aasiaantczak@gmail.com>
2018-04-05 13:13:25 +0000

    add save the list to file option

--- directory.rb
+++ directory.rb
@@ -12,6 +12,16 @@ def input_students
   end
 end
 
+def save_students
+  file = File.open("students.csv", "w")
+  @students.each do |student|
+    student_data = [student[:name], student[:cohort]]
+    csv_line = student_data.join(",")
+    file.puts csv_line
+  end
+  file.close
+end
+
 def interactive_menu
   loop do
     print_menu
@@ -22,6 +32,7 @@ end
 def print_menu
   puts "1. Input the students"
   puts "2. Show the students"
+  puts "3. Save the list to students.csv"
   puts "9. Exit"
 end
 
@@ -37,6 +48,8 @@ def process(selection)
       input_students
     when "2"
       show_students
+    when "3"
+      save_students
     when "9"
       exit
     else
