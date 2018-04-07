f8ad59af633b70ebc3db5dd528bfafebff2d80b7
asiaantczak <aasiaantczak@gmail.com>
2018-04-07 09:57:23 +0000

    add students.csv as default to load on startup, s14, ex2

--- directory.rb
+++ directory.rb
@@ -91,14 +91,11 @@ end
 
 def try_load_students
   filename = ARGV.first
-  return if filename.nil?
-  if File.exists?(filename)
+  if filename.nil?
+    filename = "students.csv"
+  end
     load_students(filename)
     puts "Loaded #{@students.count} from #{filename}"
-  else
-    puts "Sorry #{filename} doesn't exist"
-    exit
-  end
 end
 
 interactive_menu
